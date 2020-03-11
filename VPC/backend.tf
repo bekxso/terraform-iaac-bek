9041589a1b9d8d0b00fb96341439ad2f80fdae7f
Cloud User <centos@ip-172-31-24-130.eu-west-2.compute.internal>
2020-03-11 01:51:10 +0000

    added vpc

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,8 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-class-bek"
-    key    = "path/to/my/vpc"
-    region = "us-east-1"
-    #dynamodb_table = "terraform-class"
+backend "s3" {
+bucket = "terraform-class-bek"
+key = "tower/us-east-1/tools/oregon/tower.tfstate"
+region = "us-east-1"
   }
 }
